7546784c57af647021b800d41b9a20b818a43e88
Anton <140619997+8nt0n@users.noreply.github.com>
2026-04-02 20:06:12 +0200

    Create servo_test.ino

---
+++ servo_test.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(4);  // or any pin WHATEVER YOU FCKN LIKE, (i love pin 4)
+}
+
+void loop() {
+
+  myServo.write(0);
+  delay(1000);
+
+  myServo.write(90);
+  delay(1000);
+
+
+  myServo.write(180);
+  delay(1000);
+}
